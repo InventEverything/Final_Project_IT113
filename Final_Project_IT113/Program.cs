52f1a2ea8feb3a5d971db493df8307ee4066ec10
InventEverything <ian.engle@student.centralia.edu>
2024-12-07 13:59:52 -0800

    Works for my default web site. Becomes less useful on more complicated sites, and in some cases receives 403 forbidden.

--- Final_Project_IT113/Program.cs
+++ Final_Project_IT113/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Final_Project_IT113
+﻿using System.Net;
+
+namespace Final_Project_IT113
 {
     internal class Program
     {
+
+        //Which structure/algorithm/pattern you have chosen.
+            //I have decided to go with frequqncy distribution.
+        //What your project aims to demonstrate or solve.
+            //My hope was to be able to put in any web site address and receive a frequency distribution of the words used on the page. 
+            //This is partly helpful for me personaly to see a summary of which html elements are used most frequently (div, span, li, etc)
+        //In what situations would your design fail to meet expectations.
+            //I would need to do a lot of adjustments to make the information more useful. Right now my default address results as desired, but more complicated web sites 
+            //would require a lot more filtering. I also noticed that on line 54 when waiting for readkey it seems that any key works except for the "esc" key, esc seems to break
+            //my infiniute loop.
+            //I also noticed that console.clear() does not seem to clear the whole console if there is to many lines printed to the console. I am not sure how to fully clear the console.
+            //Some sites seem to return 403 forbidden error.
+        //In what situations would your design exceed expectations.
+            //I am happy to have figured out (with the help of stackoverflow.com) how to use the url parts of my code
+
         static void Main(string[] args)
         {
+            do
+            {
+                string url = "http://dog-api.kinduff.com/api/facts";
+                Console.WriteLine("Please enter a web address or press enter to use the default value: "+url);
+                do
+                {
+                    string userInput = Console.ReadLine();
+                    //if a url is added by the user
+                    if (userInput != "")
+                    {
+                        url = userInput;
+                    }
+                    //if the url is invalid
+                    if(!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Invalid entry\nPlease enter a valid web address, for example: http://dog-api.kinduff.com/api/facts");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                } while (true);
 
+                WebClient SiteSearch = new WebClient();
+                SiteSearch.BaseAddress = url;
+                string returnJson = SiteSearch.DownloadString(url);
+
+                WordList wordList = new WordList(returnJson);
+                Dictionary<string, int> OrderedList = wordList.OrderDescending();
+                Console.WriteLine("\n"+url+"\nFrequency : Word");
+                foreach (KeyValuePair<string, int> word in OrderedList)
+                {
+                    Console.WriteLine(word.Value.ToString() + " : " + word.Key.ToString());
+                }
+                Console.WriteLine("\nPress any key to start over");
+                Console.ReadKey();
+                Console.Clear();
+            } while (true);
         }
     }
 }
---
+++ Final_Project_IT113/WordList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Final_Project_IT113
+{
+    internal class WordList
+    {
+        public string BaseString { get; set; }
+        public List<string> Words { get; set; }
+        public Dictionary<string, int> FreqDist { get; set; }
+        //my stop word list is very short in comparison to class, but this was all that I felt that I needed for my default url in program
+        public static List<string> StopWords = new List<string>(["", "  ", " ", "and", "the", "a"]);
+        public WordList(string baseString)
+        {
+            BaseString = baseString;
+            Words = new List<string>();
+            FreqDist = new Dictionary<string, int>();
+
+            //I made minor adjustments to when removestopwords is called because it seemed to make a difference for when removing empty space from my dictionary keys
+            foreach(string token in WordList.RemoveStopWords(RemoveSpecialCharacters(BaseString)).ToLower().Split(" "))
+            {
+                if (FreqDist.ContainsKey(token))
+                {
+                    FreqDist[token]++;
+                }
+                else
+                {
+                    FreqDist.Add(token, 1);
+                }
+            }
+            //I feel like there is an easier way of doing this, but making a list of keys, and a list of values was the only way I could figure out how to make a list 
+            //of strings where each string is the combination of value followed by key
+            List<int> WordCount = FreqDist.Values.ToList();
+            List<string> Word = FreqDist.Keys.ToList();
+
+            foreach (int count in WordCount)
+            {
+                Words.Add(count.ToString() + " " + Word.ToString());
+
+            }
+        }
+        public Dictionary<string, int> OrderDescending()
+        {
+            return FreqDist.OrderByDescending(x => x.Value).ToDictionary();
+        }
+        public string RemoveSpecialCharacters(string str)
+        {
+            //I made all special characters that are removed be replaced with " " instead of "" to prevent words from being mixed with html elements in my dictionary keys
+            return Regex.Replace(str, "[^a-zA-Z0-9\\s]+", " ", RegexOptions.Compiled);
+        }
+        public static string RemoveStopWords(string str)
+        {
+            List<string> result = new List<string>();
+
+            result.AddRange(str.Split(" "));
+
+            result = result.Where(w => !WordList.StopWords.Contains(w)).ToList();
+
+            return String.Join(" ", result);
+        }
+    }
+}
